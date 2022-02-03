54b8b1ab56d47aca681d6e9d194590461539cda2
pc <pc@DESKTOP-GO91MJH>
2022-02-03 11:20:22 -0800

    4

---
+++ UnboxDemo/src/WrapperExample.java
@@ -0,0 +1,11 @@
+//Unboxing example of Integer to int
+public class WrapperExample {
+
+	public static void main(String[] args) {
+		Integer a=new Integer(3);
+		int i=a.intValue();
+		int j=a;
+		System.out.println(a+ " " +i+ " " +j);
+	}
+
+}
