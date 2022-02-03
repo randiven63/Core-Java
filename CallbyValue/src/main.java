2435b203a290421c2760cd37cf330d984d3d25d6
pc <pc@DESKTOP-GO91MJH>
2022-02-03 11:35:12 -0800

    15 Call by value commit

---
+++ CallbyValue/src/main.java
@@ -0,0 +1,21 @@
+//code for call by values
+
+
+class Operation
+{
+	int data=50;
+	void change(int data)
+	{
+		data=data+100;
+	}
+}
+public class main
+{
+	public static void main(String[] args) {
+		Operation op=new Operation();
+		System.out.println("before change " +op.data);
+		op.change(500);
+		System.out.println("after change " +op.data);
+	}
+
+}
