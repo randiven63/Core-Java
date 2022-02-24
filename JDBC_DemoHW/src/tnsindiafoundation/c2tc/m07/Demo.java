c6430696da00e3f5b793b0f1587d95b6b4e60369
pc <pc@DESKTOP-GO91MJH>
2022-02-23 18:19:08 -0800

    my JDBC homework

---
+++ JDBC_DemoHW/src/tnsindiafoundation/c2tc/m07/Demo.java
@@ -0,0 +1,36 @@
+package tnsindiafoundation.c2tc.m07;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Demo {
+
+	public static void main(String[] args) throws Exception{
+		String url="jdbc:mysql://localhost:3306/m07";
+		String user="root";
+		String pass="Nayna@123";
+		
+		String query="SELECT * FROM m07.details";
+		Class.forName("com.myql.cj.jdbc.Driver");
+		
+		Connection con=DriverManager.getConnection(url,user,pass);
+		Statement st=con.createStatement();
+		
+		ResultSet rs= st.executeQuery(query);
+		System.out.println("uid \t\t name \t\t age");
+		 
+        // Condiion check
+        while (rs.next()) {
+
+            int uid = rs.getInt("uid");
+            String name = rs.getString("name");
+            String age = rs.getString("age");
+            System.out.println(uid + "\t\t" + name + "\t\t" + age);
+        }
+		st.close();
+		con.close();
+	}
+
+}
